6e747af855df1ae6fca9535ac70c50205d62d17e
leonardo <sponzal4@gmail.com>
2021-09-08 18:31:00 +0200

    non funziona il maledetto migrate mannaggia

---
+++ database/migrations/2021_09_08_154358_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('country',20);
+            $table->string('city',50);
+            $table->string('hotel',15);
+            $table->date('departure');
+            $table->date('return');
+            $table->text('description');
+            $table->float('price',4,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
